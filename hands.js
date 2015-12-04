a266646a15ae8e3572b304d1f40e8aad7ef863c1
José Juan Almagro Armenteros <Jos Juan Almagro Armenteros>
2015-12-04 16:55:41 +0100

    css and divs

--- hands.js
+++ hands.js
@@ -14,7 +14,7 @@ var	xAxis = d3.svg.axis().scale(x)
 var	yAxis = d3.svg.axis().scale(y)
 	.orient("left").ticks(5);*/
 
-var svg = d3.select("#pca").append("svg")
+var svg = d3.select("#hand").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
