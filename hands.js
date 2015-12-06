fd5543869d4f4048b8a38053bcf573f248b0ef14
JoeBlair <JosephBlair@Josephs-Air.home>
2015-12-06 18:09:27 +0100

    final

--- hands.js
+++ hands.js
@@ -1,4 +1,3 @@
-
 var margin = {top:20 , right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -42,7 +41,6 @@ for (i in hand_data) {
 };
 
 
-
 var pc = [1,2];
 
 var pca_hands = d3.zip(hand_pca_data[pc[0]-1],hand_pca_data[pc[1]-1]);
@@ -51,29 +49,27 @@ var pos = {x: 250, y: 230, xh: 60, yh: 85};
 var circ_r = 200;
 var scale_h = width/3;
 
-	// Scale the range of the data
+  // Scale the range of the data
 
   var xValue = function(d) { return d;}, // data -> value
     xScale = d3.scale.linear()
-        .domain([d3.min(pca_hands, function(d) { return d[0]-0.05; }),d3.max(pca_hands, function(d) { return d[0]; })])
+        .domain([d3.min(pca_hands, function(d) { return d[0]; })/0.8,d3.max(pca_hands, function(d) { return d[0]; })*1.2])
         .range([0, width2]), // value -> display
     xMap = function(d) { return xScale(xValue(d[0]));}, // data -> display
     xAxis = d3.svg.axis().scale(xScale).orient("bottom");
 // setup y
 var yValue = function(d) { return d;}, // data -> value
     yScale = d3.scale.linear()
-        .domain([d3.min(pca_hands, function(d) { return d[1]-0.05; }), d3.max(pca_hands, function(d) { return d[1]; })])
+        .domain([d3.min(pca_hands, function(d) { return d[1]; })/0.8, d3.max(pca_hands, function(d) { return d[1]; })*1.2])
         .range([height, 0]), // value -> display
     yMap = function(d) { return yScale(yValue(d[1]));}, // data -> display
     yAxis = d3.svg.axis().scale(yScale).orient("left");
 
-
-
-	// Add the X Axis
+  // Add the X Axis
 svg2.append("g")
-		.attr("class", "x_axis")
-		.attr("transform", 'translate(0, '+ height +')')
-		.call(xAxis);
+    .attr("class", "x_axis")
+    .attr("transform", 'translate(0, '+ height +')')
+    .call(xAxis);
     svg2.append("text")
     .attr("class", "xlabel")
     .attr("x", width2 - 40)
@@ -81,10 +77,10 @@ svg2.append("g")
     .style("text-anchor", "end")
     .text("PC"+pc[0]);
 
-	// Add the Y Axis
+  // Add the Y Axis
 svg2.append("g")
-		.attr("class", "y_axis")
-		.call(yAxis);
+    .attr("class", "y_axis")
+    .call(yAxis);
   svg2.append("text")
     .attr("class", "ylabel")
     .attr("transform", "rotate(-90)")
@@ -93,58 +89,10 @@ svg2.append("g")
     .style("text-anchor", "end")
     .text("PC"+pc[1]);
 
-
-
-var scaledX = pca_hands.map(function(d){return xMap(d);}) 
-var scaledY = pca_hands.map(function(d){return yMap(d);}) 
-
-var scaleD  = d3.zip(scaledX, scaledY)
-
-var clusters = clusterfck.kmeans(scaleD, 3);
-
-
-clust1 = svg2.append("g")
-  .selectAll("path")
-    .data(d3.geom.delaunay(clusters[0]))
-  .enter().append("path")
-    .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-    .style("fill", "red")
-    .style("opacity", 0.2)
-     .on("mouseover", function(d, i){return tooltip.style("visibility", "visible").text("Cluster: " + 1), svg2.style("cursor", "pointer");})
-  .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-  .on("mouseout", function(){return tooltip.style("visibility", "hidden");});;
-
-
-clust2 = svg2.append("g")
-  .selectAll("path")
-    .data(d3.geom.delaunay(clusters[1]))
-  .enter().append("path")
-    .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-    .style("fill", "green")
-        .style("opacity", 0.2)
-
-     .on("mouseover", function(d, i){return tooltip.style("visibility", "visible").text("Cluster: " + 2), svg2.style("cursor", "pointer");})
-  .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-  .on("mouseout", function(){return tooltip.style("visibility", "hidden");});;
-
-
-clust3 = svg2.append("g")
-  .selectAll("path")
-    .data(d3.geom.delaunay(clusters[2]))
-  .enter().append("path")
-    .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-    .style("fill", "blue")
-        .style("opacity", 0.2)
-     .on("mouseover", function(d, i){return tooltip.style("visibility", "visible").text("Cluster: " + 3), svg2.style("cursor", "pointer");})
-  .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-  .on("mouseout", function(){return tooltip.style("visibility", "hidden");});;
-
-
-
 var tooltip = d3.select("#pca")
   .append("div")
   .style("background-color", "rgba(163, 171, 253, 0.6)")
-  .style("font-Family", "Impact,Charcoal,sans-serif")
+  .style("font-Family", "")
   .style("background-opacity", "0.5")
   .style("position", "absolute")
   .style("visibility", "hidden");
@@ -165,100 +113,69 @@ var handy = svg.append("g")
       .attr("stroke", "black")
       .attr("stroke-width", 1)
       .attr("opacity", "1")
-
-      .attr("fill", "rgba(238, 203, 173, 0.6)")
+      .attr("fill", "rgba(255,228, 196, 0.6)")
       .attr('transform', 'translate('+(pos.x/4-15)+','+pos.y/4+')');
 
-
-  points = svg2.selectAll('circle')
+var points = svg2.selectAll('circle')
   .data(pca_hands)
   .enter()
   .append('circle')
   .attr("cx", xMap)
   .attr("cy", yMap)
   .attr('r', 4)
-  .style("fill", "black")
   .on('click', function(d, i) {
   handy
     .transition()
-    .duration(1000)
-    .attr("d", lineFn(hands[i]));})
-
-  .on("mouseover", function(d, i){return tooltip.style("visibility", "visible").text("Hand: " + (i+1)), svg2.style("cursor", "pointer");})
+    .duration(1250)
+      .attr("d", lineFn(hands[i]));})
+  .on("mouseover", function(d, i){return tooltip.style("visibility", "visible").text("Hand: " + i), svg2.style("cursor", "pointer");})
   .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
   .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 
+    function new_cluster (data, figure, k, trans) {
+        var scaledX = data.map(function(d){return xMap(d);});
+        var scaledY = data.map(function(d){return yMap(d);});
 
-    
+        var scaleD  = d3.zip(scaledX, scaledY);
 
-  document.getElementById("button").addEventListener('click', function () {
-    var input1 = document.getElementById("pcx").value;
-    var input2 = document.getElementById("pcy").value;
+        var clusters = clusterfck.kmeans(scaleD, k);
+
+        var colors = ["red", "green", "blue", "orange", "yellow", "purple", "grey", "brown"];
+        if (trans == "new") {
+            figure.selectAll(".cluster").remove();
+        }
+        for (e = 0; e < k; e++) {
+            figure.append("g")
+                .selectAll("path")
+                .data(d3.geom.delaunay(clusters[e]))
+                .enter().append("path")
+                .attr("class", "cluster")
+                .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
+                .attr("id", e)
+                .style("fill", colors[e])
+                .style("opacity", 0.2);
+        }
+
+        svg2.selectAll("path")
+            .on("mouseover", function(d, i){return tooltip.style("visibility", "visible").text("Cluster: " + (+this.id+1)), figure.style("cursor", "pointer");})
+            .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+            .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+    }
+
+
+
+function new_pca(input1,input2) {
     var pc = [input1,input2];
     var pca_hands = d3.zip(hand_pca_data[pc[0]-1],hand_pca_data[pc[1]-1]);
+    xScale.domain([d3.min(pca_hands, function(d) { return d[0]; })/0.8,d3.max(pca_hands, function(d) { return d[0]; })*1.2]);
+    yScale.domain([d3.min(pca_hands, function(d) { return d[1]; })/0.8, d3.max(pca_hands, function(d) { return d[1]; })*1.2]);
 
-    xScale.domain([d3.min(pca_hands, function(d) { return d[0]-0.05; }),d3.max(pca_hands, function(d) { return d[0]; })]);
-    yScale.domain([d3.min(pca_hands, function(d) { return d[1]-0.05; }), d3.max(pca_hands, function(d) { return d[1]; })]);
     svg2.selectAll('circle')
         .data(pca_hands)
         .transition()
         .duration(1000)
         .attr("cx", xMap)
         .attr("cy", yMap);
-
-var scaledX = pca_hands.map(function(d){return xMap(d);}) 
-var scaledY = pca_hands.map(function(d){return yMap(d);}) 
-
-var scaleD  = d3.zip(scaledX, scaledY)
-
-var clusters = clusterfck.kmeans(scaleD, 3);
-
-clust1.remove()
-clust2.remove()
-clust3.remove()
-
-clust1 = svg2.append("g")
-  .selectAll("path")
-    .data(d3.geom.delaunay(clusters[0]))
-  .enter().append("path")
-    .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-    .style("fill", "red")
-    .style("opacity", 0.2)
-
-  .on("mouseover", function(d, i){return tooltip.style("visibility", "visible").text("Cluster: " + 1), svg2.style("cursor", "pointer");})
-  .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-  .on("mouseout", function(){return tooltip.style("visibility", "hidden");});;
-
-
-clust2 = svg2.append("g")
-  .selectAll("path")
-    .data(d3.geom.delaunay(clusters[1]))
-  .enter().append("path")
-    .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-    .style("fill", "green")
-        .style("opacity", 0.2)
-  .on("mouseover", function(d, i){return tooltip.style("visibility", "visible").text("Cluster: " + 2), svg2.style("cursor", "pointer");})
-  .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-  .on("mouseout", function(){return tooltip.style("visibility", "hidden");});;
-
-
-clust3 = svg2.append("g")
-  .selectAll("path")
-    .data(d3.geom.delaunay(clusters[2]))
-  .enter().append("path")
-    .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-    .style("fill", "blue")
-        .style("opacity", 0.2)
-     .on("mouseover", function(d, i){return tooltip.style("visibility", "visible").text("Cluster: " + 3), svg2.style("cursor", "pointer");})
-  .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-  .on("mouseout", function(){return tooltip.style("visibility", "hidden");});;
-
-
-d3.select("path")
-  .data(clusters[2])
-  .transition()
-  .duration(1000)
-
 
     svg2.select(".x_axis")
         .transition()
@@ -274,6 +191,258 @@ d3.select("path")
     svg2.select(".ylabel")
         .text("PC"+pc[1]);
 
+    return pca_hands;
+}
+
+    d3.select("#outlier")
+
+        .on("mouseover",   function(){
+            svg2.selectAll(".cluster").remove();
+            new_pca(1,2);
+            svg2.selectAll('circle')
+                .attr("r", function(d,i){
+                    if(39===i) return 6;
+                    else return 4;
+                })
+                .style('fill',function(d,i){
+                    if(39===i) return 'red';
+                    else return 'white';
+                });
+        })
+        .on("mouseout",  function () {
+                var input1 = document.getElementById("pcx").value;
+                var input2 = document.getElementById("pcy").value;
+                 svg2.selectAll(".cluster").remove();
+                new_pca(input1,input2);
+            svg2.selectAll('circle')
+                .attr("r", function(d,i){
+                    if(39===i) return 4;
+                    else return 4;
+                })
+                .style('fill',function(d,i){
+                    if(39===i) return 'white';
+                });
+        });
+
+    d3.select("#outlier2")
+
+        .on("mouseover",   function(){
+            svg2.selectAll(".cluster").remove();
+            new_pca(1,2);
+            svg2.selectAll('circle')
+                .attr("r", function(d,i){
+                    if(30===i) return 6;
+                    else return 4;
+                })
+                .style('fill',function(d,i){
+                    if(30===i) return 'red';
+                    else return 'white';
+                });
+        })
+        .on("mouseout",  function () {
+                var input1 = document.getElementById("pcx").value;
+                var input2 = document.getElementById("pcy").value;
+                 svg2.selectAll(".cluster").remove();
+                new_pca(input1,input2);
+            svg2.selectAll('circle')
+                .attr("r", function(d,i){
+                    if(30===i) return 4;
+                    else return 4;
+                })
+                .style('fill',function(d,i){
+                    if(30===i) return 'white';
+                });
+        });
+
+            d3.select("#outlier3")
+
+        .on("mouseover",   function(){
+            svg2.selectAll(".cluster").remove();
+            new_pca(1,2);
+            svg2.selectAll('circle')
+                .attr("r", function(d,i){
+                    if(35===i) return 6;
+                    else return 4;
+                })
+                .style('fill',function(d,i){
+                    if(35===i) return 'red';
+                    else return 'white';
+                });
+        })
+        .on("mouseout",  function () {
+                var input1 = document.getElementById("pcx").value;
+                var input2 = document.getElementById("pcy").value;
+                 svg2.selectAll(".cluster").remove();
+                new_pca(input1,input2);
+            svg2.selectAll('circle')
+                .attr("r", function(d,i){
+                    if(35===i) return 4;
+                    else return 4;
+                })
+                .style('fill',function(d,i){
+                    if(35===i) return 'white';
+                });
+        });
+
+            d3.select("#outlier4")
+
+        .on("mouseover",   function(){
+            svg2.selectAll(".cluster").remove();
+            new_pca(1,2);
+            svg2.selectAll('circle')
+                .attr("r", function(d,i){
+                    if(37===i) return 6;
+                    else return 4;
+                })
+                .style('fill',function(d,i){
+                    if(37===i) return 'red';
+                    else return 'white';
+                });
+        })
+        .on("mouseout",  function () {
+                var input1 = document.getElementById("pcx").value;
+                var input2 = document.getElementById("pcy").value;
+                 svg2.selectAll(".cluster").remove();
+                new_pca(input1,input2);
+            svg2.selectAll('circle')
+                .attr("r", function(d,i){
+                    if(37===i) return 4;
+                    else return 4;
+                })
+                .style('fill',function(d,i){
+                    if(37===i) return 'white';
+                });
+        });
+
+
+    document.getElementById("button1").addEventListener('click', function () {
+        var k = document.getElementById("km").value;
+        var input1 = document.getElementById("pcx").value;
+        var input2 = document.getElementById("pcy").value;
+        var pca_hands_up = new_pca(input1,input2);
+        new_cluster(pca_hands_up, svg2, k, "new");
+        d3.selection.prototype.moveToFront = function() {
+            return this.each(function(){
+                this.parentNode.appendChild(this);
+            });
+        };
+        var el = svg2.selectAll('circle');
+        el.moveToFront();
+        });
+
+  document.getElementById("button").addEventListener('click', function () {
+    var input1 = document.getElementById("pcx").value;
+    var input2 = document.getElementById("pcy").value;
+      svg2.selectAll(".cluster").remove();
+    new_pca(input1,input2);
+
 });
+
+    function calendarWeekHour(id, width, height, square, points)
+    {
+
+        var grid = d3.select(id).append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("class", "chart");
+
+        var calData = randomData(width, height, square, grid);
+
+        var row = grid.selectAll(".row")
+            .data(calData)
+            .enter().append("svg:g")
+            .attr("class", "row");
+
+        var col = row.selectAll(".cell")
+            .data(function (d) { return d; })
+            .enter().append("svg:rect")
+            .attr("class", "cell")
+            .attr("x", function(d) { return d.x; })
+            .attr("y", function(d) { return d.y; })
+            .attr("width", function(d) { return d.width; })
+            .attr("height", function(d) { return d.height; })
+            .style("fill", '#FFF')
+            .style("stroke", '#555');
+        randomData(width, height, square, grid, points);
+
+    }
+
+
+    function randomData(gridWidth, gridHeight, square, grid, points)
+    {
+        var data = new Array();
+        var gridItemWidth = gridWidth / 25;
+        var gridItemHeight = (square) ? gridItemWidth : gridHeight;
+        var startX = gridItemWidth / 2;
+        var startY = gridItemHeight / 2;
+        var stepX = gridItemWidth;
+        var stepY = gridItemHeight;
+        var xpos = startX;
+        var ypos = startY;
+        var count = 0;
+        var lineFn = d3.svg.line()
+            .x(function(d) { return d[0]*55; })
+            .y(function(d) { return d[1]*55; })
+            .interpolate("basis");
+        for (var index_a = 0; index_a < 2; index_a++)
+        {
+            data.push(new Array());
+            for (var index_b = 0; index_b < 20; index_b++)
+            {
+
+                data[index_a].push({
+                    width: gridItemWidth,
+                    height: gridItemHeight,
+                    x: xpos,
+                    y: ypos,
+                    count: count
+                });
+                grid.append("g")
+                    .append("path")
+                    .attr("d", lineFn(hands[count]))
+                    .attr("stroke", "black")
+                    .attr("stroke-width", 1)
+                    .attr("opacity", "1")
+                    .attr("fill", "rgba(255,228, 196, 0.6)")
+                    .attr("id", count)
+                    .attr('transform', 'translate('+(xpos)+','+ypos+')')
+                    .on('mouseover', function() {
+                        d3.select(this)
+                            .style('fill', "black");
+                        ind = +this.id;
+                        svg2.selectAll('circle')
+                            .attr("r", function(d,i){
+                                if(ind===i) return 6;
+                                else return 4;
+                            })
+                            .style('fill',function(d,i){
+                                if(ind===i) return 'red';
+                                else return 'white';
+                            });
+                    })
+                    .on('mouseout', function() {
+                        d3.select(this)
+                            .style("fill", "rgba(255,228, 196, 0.6)");
+                        ind = +this.id;
+                        svg2.selectAll('circle')
+                            .attr("r", function(d,i){
+                                if(ind===i) return 4;
+                                else return 4;
+                            })
+                            .style('fill',function(d,i){
+                                if(ind===i) return 'white';
+                            });
+                    })
+                xpos += stepX;
+                count += 1;
+            }
+            xpos = startX;
+            ypos += stepY;
+        }
+        return data;
+    }
+    calendarWeekHour('#all_hands', 1800, 200, true, points);
+
+
 });
 });
